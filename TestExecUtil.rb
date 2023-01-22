e51b9cafa6b3c4da21201d4c4b5cf2d6e13b32fc
hidenorly <twitte.harold@gmail.com>
2023-01-22 10:17:00 +0900

    Add test case for execCmd

---
+++ TestExecUtil.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require_relative "ExecUtil"
+
+class TestExecUtil < Minitest::Test
+	def setup
+	end
+
+	def teardown
+	end
+
+	def test_execCmd
+		puts "test_execCmd"
+		assert_equal true, ExecUtil.execCmd("/bin/sh -c echo")
+	end
+end
