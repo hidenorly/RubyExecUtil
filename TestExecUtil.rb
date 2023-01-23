79f4dde15021507e68771e116995e85208e4c6f8
hidenorly <twitte.harold@gmail.com>
2023-01-24 00:06:57 +0900

    Add test case for hasResult?

--- TestExecUtil.rb
+++ TestExecUtil.rb
@@ -13,4 +13,10 @@ class TestExecUtil < Minitest::Test
 		assert_equal true, ExecUtil.execCmd("/bin/sh -c 'echo !test_execCmd'")
 		assert_equal true, ExecUtil.execCmd("/bin/sh -c 'echo !test_execCmd'", ".", false)
 	end
+
+	def test_hasResult
+		puts "test_hasResult"
+		assert_equal true, ExecUtil.hasResult?("/bin/sh -c 'echo !test_hasResult'")
+		assert_equal false, ExecUtil.hasResult?("/bin/sh -c ''")
+	end
 end
