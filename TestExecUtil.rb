41ed10e7c5a6ecc94a994e92ff0587f16cd90ddc
hidenorly <twitte.harold@gmail.com>
2023-01-31 00:50:30 +0900

    Improve path resolve

--- TestExecUtil.rb
+++ TestExecUtil.rb
@@ -3,6 +3,9 @@ require_relative "ExecUtil"
 
 class TestExecUtil < Minitest::Test
 	def setup
+		@shPath = "/bin/sh"
+		result = ExecUtil.getExecResultEachLine("which sh")
+		@shPath = result[0] if !result.empty?
 	end
 
 	def teardown
@@ -10,19 +13,19 @@ class TestExecUtil < Minitest::Test
 
 	def test_execCmd
 		puts "test_execCmd"
-		assert_equal true, ExecUtil.execCmd("/bin/sh -c 'echo !test_execCmd'")
-		assert_equal true, ExecUtil.execCmd("/bin/sh -c 'echo !test_execCmd'", ".", false)
+		assert_equal true, ExecUtil.execCmd("#{@shPath} -c 'echo !test_execCmd'")
+		assert_equal true, ExecUtil.execCmd("#{@shPath} -c 'echo !test_execCmd'", ".", false)
 	end
 
 	def test_hasResult
 		puts "test_hasResult"
-		assert_equal true, ExecUtil.hasResult?("/bin/sh -c 'echo !test_hasResult'")
-		assert_equal false, ExecUtil.hasResult?("/bin/sh -c ''")
+		assert_equal true, ExecUtil.hasResult?("#{@shPath} -c 'echo !test_hasResult'")
+		assert_equal false, ExecUtil.hasResult?("#{@shPath} -c ''")
 	end
 
 	def test_getExecResultEachLine
 		puts "test_getExecResultEachLine"
-		assert_equal ["Ok"], ExecUtil.getExecResultEachLine("/bin/sh -c 'echo Ok'")
-		assert_equal ["1", "2"], ExecUtil.getExecResultEachLine("/bin/sh -c 'echo \"1\"; echo \"2\"'")
+		assert_equal ["Ok"], ExecUtil.getExecResultEachLine("#{@shPath} -c 'echo Ok'")
+		assert_equal ["1", "2"], ExecUtil.getExecResultEachLine("#{@shPath} -c 'echo \"1\"; echo \"2\"'")
 	end
 end
